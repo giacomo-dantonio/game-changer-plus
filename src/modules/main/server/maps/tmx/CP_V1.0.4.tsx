980745e9c3757175b22a1fc83b3dc6d356280dc9
pdcnguyen <phamdangcaonguyen@gmail.com>
2023-04-22 10:18:10 +0200

    edit map

---
+++ src/modules/main/server/maps/tmx/CP_V1.0.4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="CP_V1.0.4" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../../../../../../CP_V1.1.0_nyknck/CP_V1.0.4_nyknck/CP_V1.0.4.png" width="1024" height="1024"/>
+</tileset>
